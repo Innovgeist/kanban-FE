75ad0cdb63babd2ded517b4b49b326909d23aad0
Deepanshu <ranadee850@gmail.com>
2026-02-02 12:02:03 +0530

    dee_commit

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-export { authApi } from './auth';
+ export { authApi } from './auth';
 export { projectsApi } from './projects';
 export { boardsApi } from './boards';
 export { apiClient, setTokens, getTokens, clearTokens } from './client';
